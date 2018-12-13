d109d1b7349557414134cce76630b33e8ce1db88
Unknown <vuduykhanh3998@gmail.com>
2018-12-13 09:16:40 +0700

    update tree view

--- apps/controllers/view-car.js
+++ apps/controllers/view-car.js
@@ -3,11 +3,11 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function(req, res){
-    res.render("view-car");
+    res.render("view-car/view-car");
 });
 
 router.get("/review-car/:id", function(req, res){
-    res.render("review-car");
+    res.render("view-car/review-car");
 })
 
 module.exports = router;
