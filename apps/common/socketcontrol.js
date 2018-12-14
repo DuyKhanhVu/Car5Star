e2815a6003e4c8f7ea70ece1a2cd1d34f31a42ea
Unknown <vuduykhanh3998@gmail.com>
2018-12-14 14:08:33 +0700

    client chat

--- apps/common/socketcontrol.js
+++ apps/common/socketcontrol.js
@@ -1,5 +1,16 @@
 module.exports = function(io){
     io.sockets.on("connection", function(socket){
         console.log("have a new user connected");
+
+        //Listen send message
+        socket.on("send_message", function(message){
+            // Notify to myself
+
+            var data = {
+                sender: "You",
+                message: message
+            };
+            socket.emit("update_message", data);
+        })
     });
 }
